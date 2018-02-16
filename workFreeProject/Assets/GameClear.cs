1b3878993a43d588044a38a0dab8d05e2ca9cfc1
KadokawaRyota <ryota.creategame@gmail.com>
2018-02-16 15:05:53 +0900

    ゴールすると遷移

---
+++ workFreeProject/Assets/GameClear.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameClear : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if( col.name == "player" )
+        {
+            SceneManager.LoadScene("Result");
+        }
+    }
+}
